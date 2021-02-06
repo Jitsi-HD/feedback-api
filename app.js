81e7ecacf2abd715483dd81132911783f50a2dc7
Stean <Stean@users.noreply.github.com>
2021-02-06 18:50:56 +0100

    Fix /public path

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // use multer middleware for parsing multipart/form-data
 app.use(upload.array());
